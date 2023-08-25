7a650a89849cefe1a0143ebabb80500ff4936769
t-col-kb-2023summer <142647548+t-col-kb-2023summer@users.noreply.github.com>
2023-08-25 14:40:17 +0900

    draft: dynamic system role text changing test

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,24 +7,28 @@ role_system = st.secrets.ChatSettings.role_system
 message_max = st.secrets.ChatSettings.message_max
 
 
-# st.session_stateを使いメッセージのやりとりを保存
-if "messages" not in st.session_state:
-    st.session_state["messages"] = [
-        {"role": "system", "content": role_system}
-        ]
-if "messages_len" not in st.session_state:
-    st.session_state["messages_len"] = 0
-if "total_tokens" not in st.session_state:
-    st.session_state["total_tokens"] = 0
-if "all_tokens" not in st.session_state:
-    st.session_state["all_tokens"] = 0
+# 現在のやりとりに対するコスト表示
+def display_tokens():
+    len = str(st.session_state["messages_len"])
+    total = str(st.session_state["total_tokens"])
+    all = str(st.session_state["all_tokens"])
+    st.write("messeage数 "+len+", 今回消費token "+total+", 累計消費token "+all+"です")
+
+
+# UIの構築
+st.title("My AI Assistant")
+st.write("ChatGPT APIを使ったチャットボットです。")
+display_tokens()
 
 
 # チャットボットとやりとりする関数
 def communicate():
+    system_message_text = st.session_state["system_role_input"]
     all_messages = st.session_state["messages"]
 
+    system_message = {"role": "system", "content": system_message_text}
     user_message = {"role": "user", "content": st.session_state["user_input"]}
+    all_messages.append(system_message)
     all_messages.append(user_message)
 
     response = openai.ChatCompletion.create(
@@ -44,18 +48,16 @@ def communicate():
     st.session_state["user_input"] = ""  # 入力欄を消去
 
 
-# 現在のやりとりに対するコスト表示
-def display_tokens():
-    len = str(st.session_state["messages_len"])
-    total = str(st.session_state["total_tokens"])
-    all = str(st.session_state["all_tokens"])
-    st.write("messeage数 "+len+", 今回消費token "+total+", 累計消費token "+all+"です")
+system_role_input = st.text_input("チャットAIに適用させる設定を入力してください", key="system_role_input", on_change=communicate, value=(st.session_state["system_role_input"] if "system_role_input" in st.session_state else role_system))
 
+# st.session_stateを使いメッセージのやりとりを保存
+if "messages_len" not in st.session_state:
+    st.session_state["messages_len"] = 0
+if "total_tokens" not in st.session_state:
+    st.session_state["total_tokens"] = 0
+if "all_tokens" not in st.session_state:
+    st.session_state["all_tokens"] = 0
 
-# UIの構築
-st.title("My AI Assistant")
-st.write("ChatGPT APIを使ったチャットボットです。")
-display_tokens()
 
 user_input = st.text_input("メッセージを入力してください。", key="user_input", on_change=communicate)
 
